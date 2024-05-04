a03b8e7b5d17b5063b5d8bb200aa1be94615e1d2
amarjilani <77746571+amarjilani@users.noreply.github.com>
2024-05-03 19:58:17 -0700

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,20 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  username: "Amar Jilani",
+  title: "Hi, I'm Amar",
+  subTitle: "An aspiring software engineer driven by a passion for harnessing technology to tackle real-world challenges, with a keen focus on the intersection between machine learning and healthcare."
+  , resumeLink:
+    "", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/amarjilani",
+  linkedin: "https://www.linkedin.com/in/amarjilani/",
+  gmail: "amarjilani64@gmail.com",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -48,15 +42,15 @@ const socialMediaLinks = {
 // Skills Section
 
 const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  title: "Skills",
+  subTitle: "Comphrensive skillset in software engineering, with an emphasis on AI/ML, HPC and full-stack development.",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Skilled in designing and implementing machine learning models to address real-world challenges, leveraging data-driven approaches for impactful solutions."
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Proficient in optimizing and parallelizing high-performance code, particularly adept at addressing scientific problems with efficiency and precision."),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Experienced in full-stack development, adept at crafting robust and user-friendly applications that seamlessly integrate front-end and back-end technologies for enhanced user experiences."
     )
   ],
 
@@ -65,56 +59,36 @@ https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
     },
     {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
+      skillName: "c++",
+      fontAwesomeClassname: "fab fa-cuttlefish"
     },
     {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
     },
     {
-      skillName: "reactjs",
+      skillName: "pytorch",
+      fontAwesomeClassname: "fas fa-fire",
+    },
+    {
+      skillName: "react.js",
       fontAwesomeClassname: "fab fa-react"
     },
     {
-      skillName: "nodejs",
+      skillName: "node.js",
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
+      skillName: "sql",
       fontAwesomeClassname: "fas fa-database"
     },
     {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
+      skillName: "gcp",
+      fontAwesomeClassname: "fab fa-google"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -126,31 +100,38 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "University of California, Berkeley",
+      logo: require("./assets/images/berkeley.png"),
+      subHeader: "Master of Molecular Science and Software Engineering",
+      duration: "August 2023 - May 2024",
+      desc: "Topics: Machine Learning, High-Performance Computing, Data Science, Scientific Computing, Quantum Chemistry, Software Engineering",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
+      schoolName: "Oregon State University",
+      logo: require("./assets/images/osu.png"),
       subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      duration: "January 2022 - August 2023",
+      desc: "Topics: Data Structures, Algorithms, Operating Systems, Databases, Computer Networks, Cloud Development",
+      descBullets: []
+    },
+    {
+      schoolName: "University of Toronto, St. George",
+      logo: require("./assets/images/uoft.png"),
+      subHeader: "Honours Bachelor of Science in Neuroscience and Cell & Molecular Biology",
+      duration: "September 2016 - June 2021",
+      desc: "",
+      descBullets: []
     }
+
   ]
 };
 
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: false, //Set it to true to show Proficiency Section
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
@@ -171,7 +152,7 @@ const techStack = {
 // Work experience section
 
 const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
+  display: false, //Set it to true to show workExperiences Section
   experience: [
     {
       role: "Software Engineer",
@@ -206,35 +187,57 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
+  subtitle: "Here are some projects I've worked on!",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/lstm.jpg"),
+      projectName: "Longitudinal Alzheimer's Prediction",
+      projectDesc: "This project introduces a novel approach to Alzheimer's disease prognosis by developing an LSTM model to predict protein progression and baseline cognitive scores using neuroimaging data. By targeting the prediction of cognitive decline and biomarker evolution, the model aims to facilitate earlier diagnosis, enable personalized treatment strategies, and contribute to the development of new therapeutic interventions. This project yielded a model that demonstrates promising results in accurately forecasting the progression of tau protein concentrations across the brain and cognitive metrics.",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
+          name: "GitHub",
+          url: "https://github.com/amarjilani/capstone"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/ani.png"),
+      projectName: "ANI-1 Molecular Energy Prediction",
+      projectDesc: "This project involves the development of a design and implementation of an advanced deep learning network, leveraging the ANI-1 dataset to accurately predict molecular energies. This involved engineering a streamlined workflow for data preprocessing, model training, and evaluation. A sophisticated autoencoder network was developed to compress the sparse AEVs into dense, compressed data, which enhanced the model's generalizability.",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://nextu.se/"
+          name: "GitHub",
+          url: "https://github.com/amarjilani/ANI-1"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/ca.png"),
+      projectName: "Cellular Automata Library",
+      projectDesc: "Our team collaboratively developed a cutting-edge C++ software library specifically designed for sophisticated 2D structure modeling through cellular automata systems. This library is optimized for high performance and scalability, enabling the handling of complex computations efficiently. It includes dynamic modeling tools and integrates Python-based visualization capabilities, allowing users to intuitively analyze and visualize intricate cellular automata patterns. The design of the library features an easy-to-use API and comprehensive documentation, making it accessible to a wide range of users from various technical backgrounds and disciplines. ",
+      footerLink: [
+        {
+          name: "GitHub",
+          url: "https://github.com/amarjilani/Cellular-Automata"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/quantum.webp"),
+      projectName: "CNDO/2 Quantum Chemistry Simulation",
+      projectDesc: "This project focuses on the implementation of the CNDO/2 (Complete Neglect of Differential Overlap) method within a Self-Consistent Field (SCF) framework to calculate approximate molecular energies. By utilizing CNDO/2, we streamline quantum chemical computations, making them both accessible and efficient for predicting electronic properties of molecules. The simulation is designed to provide rapid and reliable estimations of molecular energy, crucial for understanding chemical reactivity and molecular stability. ",
+      footerLink: [
+        {
+          name: "GitHub",
+          url: "https://github.com/amarjilani/CNDO2"
         }
       ]
     }
@@ -300,7 +303,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -324,7 +327,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -343,7 +346,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +359,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -365,25 +368,25 @@ const resumeSection = {
   subtitle: "Feel free to download my resume",
 
   // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "Reach out to discuss a project, any opportunities, or just to say hi!",
+  number: "",
+  email_address: ""
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
