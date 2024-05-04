828a49204af646d9e2b0c2708fadefd4c855a05b
amarjilani <77746571+amarjilani@users.noreply.github.com>
2024-05-03 20:02:33 -0700

    fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -77,6 +77,12 @@ function Header() {
             </li>
           )}
           <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
             <a href="#contact">Contact Me</a>
           </li>
           <li>
